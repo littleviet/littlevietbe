ec2666db5c398fa673258713d371b1e563c2e13d
Nguyen Gia Hoang <hoangng71299@gmail.com>
2022-01-22 19:23:02 +0700

    Added temp xsrf files

---
+++ LittleViet.Infrastructure/Security/XSRF/UseAppXsrfExtensions.cs
@@ -0,0 +1,16 @@
+﻿using LittleViet.Infrastructure.Stripe;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Stripe;
+
+namespace LittleViet.Infrastructure.Security.XSRF;
+
+public static class UseAppXsrfExtensions
+{
+    public static IServiceCollection ConfigureXsrf(this IServiceCollection serviceCollection) 
+        => serviceCollection.AddAntiforgery(options =>
+        {
+            // options.Cookie.Name = 
+            // options.HeaderName = "X-XSRF-TOKEN";
+        });
+}
