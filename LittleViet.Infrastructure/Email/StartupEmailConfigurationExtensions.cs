820922239e89d6a8dfaffe34fb46e19b85ec1e51
Nguyen Gia Hoang <extgiahoang.nguyen@7peakssoftware.com>
2022-03-23 19:02:56 +0700

    Missing file

---
+++ LittleViet.Infrastructure/Email/StartupEmailConfigurationExtensions.cs
@@ -0,0 +1,12 @@
+﻿using LittleViet.Infrastructure.Email.Interface;
+using LittleViet.Infrastructure.Email.Service;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LittleViet.Infrastructure.Email;
+
+public static class StartupEmailConfigurationExtensions
+{
+    public static IServiceCollection AddAppEmailServices(this IServiceCollection serviceCollection) => serviceCollection
+        .AddScoped<IEmailService, EmailService>()
+        .AddScoped<ITemplateService, TemplateService>();
+}
